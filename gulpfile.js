d9e81b89582cd9d0366fe68374992c400acb4bdb
Alexander Hansson <alexander@hnssn.se>
2015-03-04 11:01:27 +0100

    Gulp

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp'),
+    sass = require('gulp-ruby-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
+
+gulp.task('express', function() {
+	var express = require('express');
+	var app = express();
+	app.use(require('connect-livereload')({port: 4002}));
+	app.use(express.static(__dirname));
+	app.listen(4000);
+});
+
+var tinylr;
+gulp.task('livereload', function() {
+	tinylr = require('tiny-lr')();
+	tinylr.listen(4002);
+});
+
+function notifyLiveReload(event) {
+  var fileName = require('path').relative(__dirname, event.path);
+
+  tinylr.changed({
+    body: {
+      files: [fileName]
+    }
+  });
+}
+
+gulp.task('styles', function() {
+	return gulp.src('scss/harpy.scss')
+	.pipe(sass({ style: 'expanded' }))
+	.pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+	.pipe(gulp.dest('css'))
+	.pipe(rename({suffix: '.min'}))
+	.pipe(minifycss())
+	.pipe(gulp.dest('css'));
+});
+
+gulp.task('watch', function() {
+	gulp.watch('scss/**/*.scss', ['styles']);
+	gulp.watch('*.html', notifyLiveReload);
+	gulp.watch('css/*.css', notifyLiveReload);
+});
+
+gulp.task('default', ['styles', 'express', 'livereload', 'watch'], function() {});
