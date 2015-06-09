eb9f05d9289604e7a8863c7bbe73fb7af76f99b3
hejhansson <hejhansson@me.com>
2015-06-09 14:57:46 +0200

    Test

--- js/min/main.js
+++ js/min/main.js
@@ -1,1 +1,2 @@
 var log=function(e){console.log(e)};!function(){"use strict";for(document.all&&!document.querySelector&&(document.body.className+=" ie7"),document.all&&document.querySelector&&!document.addEventListener&&(document.body.className+=" ie8");;){log("hello world from harpy!!!");break}}();
+//# sourceMappingURL=../maps/main.js.map
